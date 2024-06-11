39b2b5a043fddc9adebec4fcb444664f30f5a1a6
Timemi11 <momuu56@gmail.com>
2024-06-12 00:53:21 +0700

    test

--- src/model/steam.model.ts
+++ src/model/steam.model.ts
@@ -2,7 +2,7 @@ import SteamAPI from "steamapi";
 import dotenv from "dotenv";
 dotenv.config();
 
-const steam = new SteamAPI(process.env.STEAM_API || "");
+const steam = new SteamAPI("80631961452FFECB91BA8D6639E9B892");
 
 // เกมแนะนำ
 export const featuredGames = steam
